67ea91367ea3fc80e52a5d7197af60c03fb33c08
CyberDanOsgood <CyberDanOsgood@outlook.com>
2025-09-25 21:24:53 -0800

    Update WN10-AC-000005.ps1

--- STIGS/WN10-AC-000005.ps1
+++ STIGS/WN10-AC-000005.ps1
@@ -1,2 +1,31 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Daniel Osgood
+    LinkedIn        : linkedin.com/in/daniel-osgood-672866289/
+    GitHub          : github.com/CyberDanOsgood/LOGN_PACIFIC
+    Date Created    : 2025-09-25
+    Last Modified   : 2025-09-25
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AU-000500.ps1 
+#>
+
+# Run as admin
+
 # Set account lockout duration to 15 minutes
 net accounts /lockoutduration:15
